d27c30b9f9742a012e9691b9d1f9d77b1e62fde5
Csaba Fülöp <csfulop@users.noreply.github.com>
2019-12-04 22:09:39 +0100

    Better solution for the minimum depth of binary tree problem with level order traversal

--- src/main/java/hu/fcs/algorithms/tree/MinimumDepth.java
+++ src/main/java/hu/fcs/algorithms/tree/MinimumDepth.java
@@ -1,5 +1,8 @@
 package hu.fcs.algorithms.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import static java.lang.Math.min;
 
 /**
@@ -20,5 +23,42 @@ public class MinimumDepth {
                 return min(of(tree.getA()), of(tree.getB())) + 1;
             }
         }
+    }
+
+    /**
+     * Better solution with Level Order Traversal
+     *
+     * @param tree
+     * @return
+     */
+    public static int of2(BinaryTree tree) {
+        class Item {
+            int depth;
+            BinaryTree tree;
+
+            public Item(int depth, BinaryTree tree) {
+                this.depth = depth;
+                this.tree = tree;
+            }
+        }
+        Queue<Item> items = new LinkedList<>();
+        if (tree == null) {
+            return 0;
+        } else {
+            items.add(new Item(1, tree));
+        }
+        while (items.size() > 0) {
+            Item current = items.remove();
+            if (current.tree.getA() == null && current.tree.getB() == null) {
+                return current.depth;
+            }
+            if (current.tree.getA() != null) {
+                items.add(new Item(current.depth + 1, current.tree.getA()));
+            }
+            if (current.tree.getB() != null) {
+                items.add(new Item(current.depth + 1, current.tree.getB()));
+            }
+        }
+        throw new RuntimeException();
     }
 }
--- src/test/java/hu/fcs/algorithms/tree/MinimumDepthTest.java
+++ src/test/java/hu/fcs/algorithms/tree/MinimumDepthTest.java
@@ -2,6 +2,8 @@ package hu.fcs.algorithms.tree;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.Function;
+
 import static hu.fcs.algorithms.tree.BinaryTree.bt;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -9,13 +11,22 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class MinimumDepthTest {
     @Test
     public void testMinimumdepth() {
-        assertThat(MinimumDepth.of(null), is(0));
-        assertThat(MinimumDepth.of(bt()), is(1));
-        assertThat(MinimumDepth.of(bt(bt())), is(2));
-        assertThat(MinimumDepth.of(bt(null, bt())), is(2));
-        assertThat(MinimumDepth.of(bt(bt(), bt())), is(2));
-        assertThat(MinimumDepth.of(bt(bt(bt()))), is(3));
-        assertThat(MinimumDepth.of(bt(bt(bt()), bt())), is(2));
+        testMinimumDepthFor(MinimumDepth::of);
+    }
+
+    @Test
+    public void testMinimumdepth2() {
+        testMinimumDepthFor(MinimumDepth::of2);
+    }
+
+    private void testMinimumDepthFor(Function<BinaryTree, Integer> f) {
+        assertThat(f.apply(null), is(0));
+        assertThat(f.apply(bt()), is(1));
+        assertThat(f.apply(bt(bt())), is(2));
+        assertThat(f.apply(bt(null, bt())), is(2));
+        assertThat(f.apply(bt(bt(), bt())), is(2));
+        assertThat(f.apply(bt(bt(bt()))), is(3));
+        assertThat(f.apply(bt(bt(bt()), bt())), is(2));
     }
 
 
